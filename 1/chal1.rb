1b1bbab3982afd0fc6776124f41509eb7d236343
ElliottDG <elliottdgregory@gmail.com>
2023-12-02 16:12:40 +0000

    TODO need to finish

--- 1/chal1.rb
+++ 1/chal1.rb
@@ -7,5 +7,19 @@ def getNumbers
     return s
 end
 
+def getNumbers2
+    s = []
+    numbers = ['one','two','three','four','five','six','seven','eight','nine']
+    File.open('input1.txt').each do |line|
+        nums = line.scan(/[one|two|three|four|five|six|seven|eight|nine]/)
+        s.push (numbers.find_index(nums.first).to_i + 1 * 10) + (numbers.find_index(nums.last).to_+1)
+    end    
+    return s
+end
+
+# pt1
 puts getNumbers.sum
+
+# pt2
+puts getNumbers2.sum
 
