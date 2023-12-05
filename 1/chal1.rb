d3b81e0fc59aeb615fa102949bd1047be1c4a925
ElliottDG <elliottdgregory@gmail.com>
2023-12-05 13:25:59 +0000

    day 1 - probs coudl refactor

--- 1/chal1.rb
+++ 1/chal1.rb
@@ -7,14 +7,34 @@ def getNumbers
     return s
 end
 
+NUMS = {
+"1" => "1",
+  "2" => "2",
+  "3" => "3",
+  "4" => "4",
+  "5" => "5",
+  "6" => "6",
+  "7" => "7",
+  "8" => "8",
+  "9" => "9",
+  "one" => "1",
+  "two" => "2",
+  "three" => "3",
+  "four" => "4",
+  "five" => "5",
+  "six" => "6",
+  "seven" => "7",
+  "eight" => "8",
+  "nine" => "9",
+}
+
 def getNumbers2
     s = []
     numbers = ['one','two','three','four','five','six','seven','eight','nine']
-    File.open('input1.txt').each do |line|
-        nums = line.scan(/[one|two|three|four|five|six|seven|eight|nine]/)
-        s.push (numbers.find_index(nums.first).to_i + 1 * 10) + (numbers.find_index(nums.last).to_+1)
-    end    
-    return s
+    File.open('input1.txt').each.map do |line|
+        nums = line.scan(/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/)
+        (NUMS[nums.first[0]] + NUMS[nums.last[0]]).to_i
+    end
 end
 
 # pt1
@@ -22,4 +42,3 @@ puts getNumbers.sum
 
 # pt2
 puts getNumbers2.sum
-
